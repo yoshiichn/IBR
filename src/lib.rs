002d816364cb4a1a5d30de8e3336aef79440b3f8
youzhen0x38 <24409573+youzhen0x38@users.noreply.github.com>
2023-02-27 19:06:19 +0900

    update/びゅう~

--- src/lib.rs
+++ src/lib.rs
@@ -10,19 +10,19 @@ struct Organization {
     reviewers: Vec<Reviewer>,
     repositories: Vec<Repository>,
 }
-
+ 
 #[derive(Debug, Deserialize)]
 struct Repository {
     name: String,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize)]
 struct Reviewer {
     name: String,
     assigned_pull_requests: Vec<PullRequest>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize)]
 struct PullRequest {
     id: String,
     repo_name: String,
@@ -68,135 +68,150 @@ fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
 }
 
 async fn fetch_organization_data() -> Result<Organization> {
-    let organization_name = "your-organization";
-    let access_token = "your-access-token";
-    let mut headers = HeaderMap::new();
-    headers.insert(
-        header::AUTHORIZATION,
-        format!("Bearer {}", access_token).parse().unwrap(),
-    );
-    headers.insert(header::USER_AGENT, "my-app".parse().unwrap());
-    // セッションを再利用して複数回リクエストするためのインスタンスを生成する
-    let client = reqwest::Client::new();
-    let repositories_url = format!("https://api.github.com/orgs/{}/repos", organization_name);
-    let repositories_response = &client
-        .get(&repositories_url)
-        .headers(headers.clone())
-        .send()
-        .await
-        .with_context(|| format!("Failed to fetch repositories from {}", repositories_url))?
-        .text()
-        .await
-        .with_context(|| "Failed to parse repositories response")?;
-    let mut repos: Vec<Repository> =
-        serde_json::from_str(&repositories_response).unwrap_or_else(|_| Vec::new());
-    for repository in &mut repos {
-        let pulls_url = format!(
-            "https://api.github.com/repos/{}/{}/pulls?state=open",
-            organization_name, repository.name
-        );
-        let pulls_response = &client
-            .get(&pulls_url)
-            .headers(headers.clone())
-            .send()
-            .await
-            .with_context(|| format!("Failed to fetch pull requests from {}", pulls_url))?
-            .text()
-            .await
-            .with_context(|| "Failed to parse pull requests response")?;
-        let pulls: Vec<serde_json::Value> = serde_json::from_str(&pulls_response)
-            .with_context(|| "Failed to parse pull requests")?;
-        for pull in pulls {
-            let reviews_url = pull["url"]
-                .as_str()
-                .unwrap()
-                .replace("api.", "")
-                .replace("/pulls/", "/pulls/")
-                + "/reviews";
-            let reviews_response = &client
-                .get(&reviews_url)
-                .headers(headers.clone())
-                .send()
-                .await
-                .with_context(|| format!("Failed to fetch reviews from {}", reviews_url))?
-                .text()
-                .await
-                .with_context(|| "Failed to parse reviews response")?;
-            let reviews: Vec<serde_json::Value> = serde_json::from_str(&reviews_response)
-                .with_context(|| "Failed to parse reviews")?;
-            for review in reviews {
-                let reviewer_login = review["user"]["login"].as_str().unwrap().to_string();
-                let state = review["state"].as_str().unwrap().to_string();
-
-                // if state != "COMMENTED" && state != "DISMISSED" {
-                //     if let Some(reviewer) = repository.find(|r| r.name == reviewer_login) {
-                //         reviewer
-                //             .assigned_pull_requests
-                //             .push(pull["url"].as_str().unwrap().to_string());
-                //     } else {
-                //         repository.reviewers.push(Reviewer {
-                //             name: reviewer_login.clone(),
-                //             assigned_pull_requests: vec![pull["url"].as_str().unwrap().to_string()],
-                //         });
-                //     }
-                // }
-            }
-        }
-    }
-
-    let reviewers = vec![];
-    let repositories = vec![];
+    let pr1 = PullRequest {
+        id: "id-1".to_string(),
+        repo_name: "repo1".to_string(),
+    };
+    let pr2 = PullRequest {
+        id: "id-2".to_string(),
+        repo_name: "repo2".to_string(),
+    };
+    let pr3 = PullRequest {
+        id: "id-3".to_string(),
+        repo_name: "repo1".to_string(),
+    };
+    let reviewer1 = Reviewer {
+        name: "user-1".to_string(),
+        assigned_pull_requests: vec![pr1.clone(), pr2.clone(), pr3.clone()],
+    };
+    let reviewer2 = Reviewer {
+        name: "user-2".to_string(),
+        assigned_pull_requests: vec![pr1.clone(), pr2.clone()],
+    };
     Ok(Organization {
-        name: organization_name.to_string(),
-        reviewers,
-        repositories,
+        name: "my-org".to_string(),
+        reviewers: vec![reviewer1, reviewer2],
+        repositories: vec![Repository {name: "repo1".to_string()}, Repository {name: "repo2".to_string()}]
     })
 }
 
 fn view(model: &Model) -> Node<Msg> {
     div![
-        h1!("GitHub Organization Reviewers"),
-        button!["Fetch data", ev(Ev::Click, |_| Msg::FetchData),],
-        // match &model.organization {
-        //     Some(organization) => {
-        //         div![
-        //             p![format!("Organization: {}", organization.name)],
-        //             div![
-        //                 C!["reviewers"],
-        //                 organization.name.chars().map(|repository| {
-        //                     div![
-        //                         h2![&repository.name],
-        //                         div![
-        //                             C!["reviewers"],
-        //                             repository.reviewers.iter().map(|reviewer| {
-        //                                 div![
-        //                                     C!["reviewer"],
-        //                                     p![&reviewer.name],
-        //                                     div![
-        //                                         C!["pull-requests"],
-        //                                         reviewer.assigned_pull_requests.iter().map(|url| {
-        //                                             a![
-        //                                                 attrs! {
-        //                                                 At::Href => url,
-        //                                                 },
-        //                                                 &url
-        //                                             ]
-        //                                         }),
-        //                                     ],
-        //                                 ]
-        //                             }),
-        //                         ],
-        //                     ]
-        //                 }),
-        //             ],
-        //         ]
-        //     }
-        //     None => {
-        //         match &model.error_message {
-        //             Some(error_message) => p![error_message],
-        //             None => p!["Click the button to fetch data."],
-        //         }
-        //     }
-        // }
+        h1![
+            a![
+                attrs! {
+                    At::Href => "https://github.com/yoshiichn/IBR",
+                    At::Target => "_blank",
+                    At::Rel => "noopener noreferrer",
+                },
+                format!("{} I'm Busy Reviewing. {}", '\u{1F347}', '\u{1F980}')
+            ]
+        ],
+        button![
+            "Fetch data",
+            ev(Ev::Click, |_| Msg::FetchData),
+            style![
+                St::BackgroundColor => "#2c3e50",
+                St::Color => "#ffffff",
+                St::Padding => "10px 20px",
+                St::BorderRadius => "5px",
+                St::Cursor => "pointer",
+            ],
+        ],
+        match &model.organization {
+            Some(organization) => {
+                div![
+                    p![
+                        style![
+                            St::FontWeight => "bold",
+                            St::FontSize => "18px",
+                            St::MarginBottom => "10px",
+                        ],
+                        format!("Organization: {}", organization.name)
+                    ],
+                    table![
+                        style![
+                            St::BorderCollapse => "collapse",
+                            St::Width => "100%",
+                            St::MarginBottom => "20px",
+                        ],
+                        thead![
+                            style![
+                                St::BackgroundColor => "#2c3e50",
+                                St::Color => "#ffffff",
+                            ],
+                            tr![
+                                style![
+                                    St::FontWeight => "bold",
+                                    St::Padding => "10px",
+                                    St::TextAlign => "left",
+                                ],
+                                th!["Users"],
+                                organization.repositories.iter().map(|repo| {
+                                    th![
+                                        style![
+                                            St::Padding => "10px",
+                                            St::TextAlign => "center",
+                                        ],
+                                        &repo.name
+                                    ]
+                                })
+                            ]
+                        ],
+                        tbody![
+                            organization.reviewers.iter().map(|reviewer| {
+                                tr![
+                                    td![
+                                        style![
+                                            St::Padding => "10px",
+                                            St::VerticalAlign => "top",
+                                        ],
+                                        &reviewer.name
+                                    ],
+                                    organization.repositories.iter().map(|repo| {
+                                        let prs: Vec<PullRequest> = reviewer.assigned_pull_requests.iter().filter(|pr| pr.repo_name == *repo.name).cloned().collect();
+                                        td![
+                                            style![
+                                                St::Padding => "10px",
+                                                St::VerticalAlign => "top",
+                                                St::TextAlign => "center",
+                                            ],
+                                            prs.iter().map(|pr| {
+                                                a![
+                                                    style![
+                                                        St::BackgroundColor => "#3498db",
+                                                        St::Color => "#ffffff",
+                                                        St::TextDecoration => "none",
+                                                        St::Padding => "5px 10px",
+                                                        St::BorderRadius => "5px",
+                                                        St::Cursor => "pointer",
+                                                    ],
+                                                    &pr.id
+                                                ]
+                                            })
+                                        ]
+                                    })
+                                ]
+                            })
+                        ]
+                    ]
+                ]
+            }
+            None => match &model.error_message {
+                Some(error_message) => p![
+                    style![
+                        St::FontWeight => "bold",
+                        St::Color => "red",
+                    ],
+                    error_message
+                ],
+                None => p![
+                    style![
+                        St::FontWeight => "bold",
+                    ],
+                    "Click the button to fetch data."
+                ],
+            },
+        }
     ]
 }
