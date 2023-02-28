3c4f28796888c6c8b3353fc8bcb638f3de2465bf
youzhen0x38 <24409573+youzhen0x38@users.noreply.github.com>
2023-02-28 12:20:21 +0900

    update/びゅう~

--- src/lib.rs
+++ src/lib.rs
@@ -155,48 +155,121 @@ async fn fetch_organization_data() -> Result<Organization> {
 
 fn view(model: &Model) -> Node<Msg> {
     div![
-        h1!("GitHub Organization Reviewers"),
-        button!["Fetch data", ev(Ev::Click, |_| Msg::FetchData),],
-        // match &model.organization {
-        //     Some(organization) => {
-        //         div![
-        //             p![format!("Organization: {}", organization.name)],
-        //             div![
-        //                 C!["reviewers"],
-        //                 organization.name.chars().map(|repository| {
-        //                     div![
-        //                         h2![&repository.name],
-        //                         div![
-        //                             C!["reviewers"],
-        //                             repository.reviewers.iter().map(|reviewer| {
-        //                                 div![
-        //                                     C!["reviewer"],
-        //                                     p![&reviewer.name],
-        //                                     div![
-        //                                         C!["pull-requests"],
-        //                                         reviewer.assigned_pull_requests.iter().map(|url| {
-        //                                             a![
-        //                                                 attrs! {
-        //                                                 At::Href => url,
-        //                                                 },
-        //                                                 &url
-        //                                             ]
-        //                                         }),
-        //                                     ],
-        //                                 ]
-        //                             }),
-        //                         ],
-        //                     ]
-        //                 }),
-        //             ],
-        //         ]
-        //     }
-        //     None => {
-        //         match &model.error_message {
-        //             Some(error_message) => p![error_message],
-        //             None => p!["Click the button to fetch data."],
-        //         }
-        //     }
-        // }
+        h1![
+            a![
+                attrs! {
+                    At::Href => "https://github.com/yoshiichn/IBR",
+                    At::Target => "_blank",
+                    At::Rel => "noopener noreferrer",
+                },
+                format!("{} I'm Busy Reviewing. {}", '\u{1F347}', '\u{1F980}')
+            ]
+        ],
+        button![
+            "Fetch data",
+            ev(Ev::Click, |_| Msg::FetchData),
+            style![
+                St::BackgroundColor => "#2c3e50",
+                St::Color => "#ffffff",
+                St::Padding => "10px 20px",
+                St::BorderRadius => "5px",
+                St::Cursor => "pointer",
+            ],
+        ],
+        match &model.organization {
+            Some(organization) => {
+                div![
+                    p![
+                        style![
+                            St::FontWeight => "bold",
+                            St::FontSize => "18px",
+                            St::MarginBottom => "10px",
+                        ],
+                        format!("Organization: {}", organization.name)
+                    ],
+                    table![
+                        style![
+                            St::BorderCollapse => "collapse",
+                            St::Width => "100%",
+                            St::MarginBottom => "20px",
+                        ],
+                        thead![
+                            style![
+                                St::BackgroundColor => "#2c3e50",
+                                St::Color => "#ffffff",
+                            ],
+                            tr![
+                                style![
+                                    St::FontWeight => "bold",
+                                    St::Padding => "10px",
+                                    St::TextAlign => "left",
+                                ],
+                                th!["Users"],
+                                organization.repositories.iter().map(|repo| {
+                                    th![
+                                        style![
+                                            St::Padding => "10px",
+                                            St::TextAlign => "center",
+                                        ],
+                                        &repo.name
+                                    ]
+                                })
+                            ]
+                        ],
+                        tbody![
+                            organization.reviewers.iter().map(|reviewer| {
+                                tr![
+                                    td![
+                                        style![
+                                            St::Padding => "10px",
+                                            St::VerticalAlign => "top",
+                                        ],
+                                        &reviewer.name
+                                    ],
+                                    organization.repositories.iter().map(|repo| {
+                                        let prs: Vec<PullRequest> = reviewer.assigned_pull_requests.iter().filter(|pr| pr.repo_name == *repo.name).cloned().collect();
+                                        td![
+                                            style![
+                                                St::Padding => "10px",
+                                                St::VerticalAlign => "top",
+                                                St::TextAlign => "center",
+                                            ],
+                                            prs.iter().map(|pr| {
+                                                a![
+                                                    style![
+                                                        St::BackgroundColor => "#3498db",
+                                                        St::Color => "#ffffff",
+                                                        St::TextDecoration => "none",
+                                                        St::Padding => "5px 10px",
+                                                        St::BorderRadius => "5px",
+                                                        St::Cursor => "pointer",
+                                                    ],
+                                                    &pr.id
+                                                ]
+                                            })
+                                        ]
+                                    })
+                                ]
+                            })
+                        ]
+                    ]
+                ]
+            }
+            None => match &model.error_message {
+                Some(error_message) => p![
+                    style![
+                        St::FontWeight => "bold",
+                        St::Color => "red",
+                    ],
+                    error_message
+                ],
+                None => p![
+                    style![
+                        St::FontWeight => "bold",
+                    ],
+                    "Click the button to fetch data."
+                ],
+            },
+        }
     ]
 }
